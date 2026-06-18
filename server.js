2aacc0c0aedc22ebea81e94f6be71150ec52346c
Doctype44 <vb3777735@gmail.com>
2026-06-18 18:02:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -340,7 +340,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* =========================
    SERVER
 ========================= */
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
